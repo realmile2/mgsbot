929526d3b6405d358da691e17b326ca9e6c03809
realmile2 <47159364+realmile2@users.noreply.github.com>
2019-02-09 17:26:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,4 +49,4 @@ function multiplyCommand(arguments, receivedMessage) {
     receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
 }
 
-client.login("XXXXXXXXXXX") // Replace XXXXX with your bot token
+client.login("BOT-TOKEN") // Replace XXXXX with your bot token
