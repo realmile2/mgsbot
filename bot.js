57de6ec66cfe54d65fdf9c53f6a3afed76c8aaa3
realmile2 <47159364+realmile2@users.noreply.github.com>
2019-02-09 17:41:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ function helpCommand(arguments, receivedMessage) {
     if (arguments.length > 0) {
         receivedMessage.channel.send("It looks like you might need help with " + arguments)
     } else {
-        receivedMessage.channel.send("Commands `!ping`\n`!no u`\n")
+        receivedMessage.channel.send("Commands: \n`!ping`\n`!no u`\n")
     }
 }
 
